e75b51a75476b653b5bbfac90f3e9e0c6f3d1f6f
Micah <migregorio@users.noreply.github.com>
2017-05-01 15:17:23 -0700

    Create tensify.js

---
+++ tensify.js
@@ -0,0 +1,24 @@
+// Dictionary of words
+// Key = Present tense
+// Value = Past tense
+var dictionary = {
+	"are": "were",
+	"eat": "ate",
+	"go": "went",
+	"have": "had",
+	"inherit": "inherited",
+	"is": "was",
+	"run": "ran",
+	"sit": "sat",
+	"visit": "visited"
+};
+
+// Convert string to past tense
+String.prototype.toPastTense = function () {
+    //
+};
+
+// Convert string to present tense
+String.prototype.toPresentTense = function () {
+    //
+};
